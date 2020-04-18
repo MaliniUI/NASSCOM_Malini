2e8ba3267a649a87e095fc9ea7068ca06592ee6e
MaliniUI <malinibe2010@gmail.com>
2020-04-18 15:13:45 +0530

    2 files modified, 3 added

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,10 +17,11 @@ import {
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RegisterPassComponent } from './register-pass/register-pass.component';
+import { RegisterIndividualPassComponent } from './landing-page/register-individual-pass/register-individual-pass.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MdToolbarModule, MdTabsModule, MdButtonModule, MdInputModule, MdDatepickerModule, MdNativeDateModule, MdCheckboxModule, MdRadioModule, AppRoutingModule ],
-  declarations: [ AppComponent, LandingPageComponent,  RegisterPassComponent ],
+  declarations: [ AppComponent, LandingPageComponent,  RegisterPassComponent, RegisterIndividualPassComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/landing-page/register-individual-pass/register-individual-pass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-individual-pass',
+  templateUrl: './register-individual-pass.component.html',
+  styleUrls: ['./register-individual-pass.component.css']
+})
+export class RegisterIndividualPassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
