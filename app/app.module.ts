ccf2f26e0690d3659042458f7474e49f0f7ecf7a
MaliniUI <malinibe2010@gmail.com>
2020-04-18 15:36:42 +0530

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -16,7 +16,6 @@ import {
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { RegisterIndividualPassComponent } from './landing-page/register-individual-pass/register-individual-pass.component';
 import { RegisterOrganisationPassComponent } from './register-organisation-pass/register-organisation-pass.component';
 import { RegisterSkillLabourPassComponent } from './register-skill-labour-pass/register-skill-labour-pass.component';
 import { RegisteredPassComponent } from './registered-pass/registered-pass.component';
--- app/app.router.ts
+++ app/app.router.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {Routes,RouterModule} from '@angular/router';
 import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {RegisterPassComponent} from "./register-pass/register-pass.component";
-import { RegisterIndividualPassComponent } from './landing-page/register-individual-pass/register-individual-pass.component';
 import { RegisterOrganisationPassComponent } from './register-organisation-pass/register-organisation-pass.component';
 import { RegisterSkillLabourPassComponent } from './register-skill-labour-pass/register-skill-labour-pass.component';
 import { RegisteredPassComponent } from './registered-pass/registered-pass.component';
